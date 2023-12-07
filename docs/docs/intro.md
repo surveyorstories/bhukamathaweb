80f434186ceab0188a561946a498ca075639acd0
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-07 12:39:37 +0530

    adding docs

---
+++ docs/docs/intro.md
@@ -0,0 +1,48 @@
+---
+sidebar_position: 1
+---
+
+# Welcome
+
+Let's discover **Bhukamtha**.
+
+Welcome to the Bhukamatha Docs We're your go-to source for all things related to the Bhukamatha plugin, designed specifically for QGIS in Mind of Resurvey. Our mission is to assist you in successfully completing the AP Resurvey 2020 project by providing comprehensive resources, updates, and support. Explore our site to unlock the full potential of this powerful tool!
+
+## Getting Started
+
+Get started by **Downloading** the Plugin from Bhukamatha web or from here
+
+:::warning **🌵 Prerequisites**
+
+**Bhukamatha** requires **QGIS version 3.26** or a newer version to function properly.
+**Gautami** telugu font need to be installed
+
+:::
+
+## Installation
+
+### 1. Download the Plugin
+
+- Click on the download button
+- Locate the plugin file you want to install in your PC(Laptop).
+- The plugin file is usually in ZIP file format
+
+### 2. Access QGIS Plugin Manager
+
+- Launch the QGIS(>2.26) application on your computer.
+- In the QGIS menu, go to "Plugins" Click on "Manage and Install Plugins.
+- In the Plugin Manager window, go to the "Installed" tab.
+
+### 3. Install the Plugin
+
+- Click on the "Install from ZIP" button.
+- Browse your computer to select the Bhukamatha plugin ZIP file you want to install.
+- Click "Install Plugin."
+- Once installed, you may need to enable the plugin by checking the box next to its name.
+- Close the Plugin Manager.
+
+:::tip success
+
+Yeah, you did it! 🚀 You're now armed with incredible new tools at your fingertips. Let the adventure begin!
+
+:::
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,139 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Surveyor Stories",
+  tagline: "A Tool to Discover ",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://github.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/bhukamathaweb/docs",
+  deploymentBranch: "gh-pages",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "Surveyor Stories", // Usually your GitHub org/user name.
+  projectName: "bhukamathaweb", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/surveyorstories",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/surveyorstories/bhukamathaweb",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Surveyor stories",
+        logo: {
+          alt: "Surveyor stories",
+          src: "img/icon.webp",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/surveyorstories",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          // {
+          //   title: "Docs",
+          //   items: [
+          //     {
+          //       label: "Tutorial",
+          //       to: "/docs/intro",
+          //     },
+          //   ],
+          // },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Telegram",
+                href: "https://t.me/surveyorstories",
+              },
+              {
+                label: "YouTube",
+                href: "https://www.youtube.com/@surveyorstories",
+              },
+              // {
+              //   label: "Twitter",
+              //   href: "https://twitter.com/docusaurus",
+              // },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
+              {
+                label: "GitHub",
+                href: "https://github.com/surveyorstories",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Surveyor Stories. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
